df7da7741293b68bc4e587113207373d95b6306f
michaeldlamini <michaelmini67@gmail.com>
2025-06-17 22:39:40 +0200

    Create numeric-data.py

---
+++ numeric-data.py
@@ -0,0 +1,19 @@
+#Operators
+
+#Addition (+)
+#Subtraction (-)
+#Multiplication (*)
+#Division (/)
+#Modulus (%)
+#Exponent (**)
+
+x = 6
+y = 7 + x
+
+print(x+y)
+print(y-x)
+print(x*y)
+print(y/x)
+print(y%x)
+print(x%y)
+print(x**y)
