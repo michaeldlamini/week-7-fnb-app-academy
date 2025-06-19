192f19c2e5125f60d540970e14ef41c5786ad44f
michaeldlamini <michaelmini67@gmail.com>
2025-06-19 11:22:22 +0200

    Create loops-2.py

---
+++ loops-2.py
@@ -0,0 +1,11 @@
+#Loop control statements
+
+count = 0
+
+while count <6:
+    print(count)
+    count += 1
+    if count == 3:
+        break #Exists the loop when the count is reached - 3
+    
+   
